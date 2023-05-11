315a54dd2041407138279f3126bfa986849223c5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-11 19:46:10 +0600

    hero section responsive

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,10 +2,10 @@ import HomeHero from "../../assets/homeHero.png";
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-between lg:px-24 lg:pt-11 lg:mt-16">
-      <div>
-        <h1 className="text-5xl font-bold">Economisez du temp et de l’argent avec votre logiciel Smili food</h1>
-        <p className="text-2xl text-[#072B38] mt-5 italic font-light">Simplifiez votre activité de restauration en optimisant chaque étape et concentrez-vous sur le développement de votre restaurant.</p>
+    <div className="flex flex-col-reverse lg:flex-row items-center justify-between px-16 pt-5 mt-8 md:mt-10 md:px-20 md:pt-8 lg:px-24 lg:pt-11 lg:mt-16">
+      <div className="lg:mr-5">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Economisez du temp et de l’argent avec votre logiciel Smili food</h1>
+        <p className="text-lg md:text-xl lg:text-2xl text-[#072B38] mt-5 italic font-light">Simplifiez votre activité de restauration en optimisant chaque étape et concentrez-vous sur le développement de votre restaurant.</p>
       </div>
       <img src={HomeHero} alt="home hero image" />
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,8 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      {/* <Hero />
-      <Services />
+      <Hero />
+      {/* <Services />
       <Versatile />
       <Support />
       <Testimonial />
